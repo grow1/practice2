1d2c02b2fd909edb4ee243e1c9c5da2142f961ff
Iryna-Smirnova <irina_smirnova48@ukr.net>
2023-03-05 11:40:19 +0200

    task-3

--- script.js
+++ script.js
@@ -31,14 +31,43 @@
  *? Якщо об'єкт salaries порожній, то результат має бути 0
  */
 
- const salaries = {
-  Mango: 100,
-  Poly: 160,
-  Ajax: 1470,
-};
-const  salariesArray = Object.values(salaries)
-let sum = 0;
-for (const item of salariesArray){
-  sum += item;
+//  const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
+// const  salariesArray = Object.values(salaries)
+// let sum = 0;
+// for (const item of salariesArray){
+//   sum += item;
+// }
+// console.log(sum);
+
+
+
+/**
+ *? Напишіть ф-цію calcTotalPrice(stones, stonesName),
+ *? яка приймає масив об'єктів та
+ *? рядок під назвою каменю.
+ *? Функція вважає та повертає загальну вартість каменів
+ *? з таким ім'ям, ціною та кількістю з об'єкта
+ */
+
+const stones = [
+  { name: "Изумруд", price: 1300, quantity: 4 },
+  { name: "Бриллиант", price: 2700, quantity: 6 },
+  { name: "Сапфир", price: 400, quantity: 7 },
+  { name: "Щебень", price: 150, quantity: 100 },
+];
+
+const calcTotalPrice = (arr, stonesName) => {
+    for (const item of arr) {
+        
+ if(item.name === stonesName) {
+    return item.price * item.quantity
+ }
+    }
+
 }
-console.log(sum);
+
+console.log(calcTotalPrice(stones, "Изумруд"));
