e6f4fdfe5a2a120b18e0e3f0e4de98c87abb2901
Viktoriah0307 <viktoriah0307@gmail.com>
2023-03-05 11:12:22 +0200

    task second

--- script.js
+++ script.js
@@ -8,18 +8,37 @@
  *? ключ: значення використовуючи Object.keys() і for...of
  */
 
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tennis",
-  premium: true,
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tennis",
+//   premium: true,
+// };
+
+// user.mood = 'happy';
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// const ourKeys = Object.keys(user)
+// for (const key of ourKeys) {
+//     console.log(`${key}: ${user[key]}`)
+// }
+/**
+ *? У нас є об'єкт, у якому зберігаються зарплати
+ *? нашої команди
+ *? Напишіть код для підсумовування всіх зарплат і
+ *? збережіть його результат у змінній sum.
+ *? Якщо об'єкт salaries порожній, то результат має бути 0
+ */
+
+ const salaries = {
+  Mango: 100,
+  Poly: 160,
+  Ajax: 1470,
 };
-
-user.mood = 'happy';
-user.hobby = "skydiving";
-user.premium = false;
-
-const ourKeys = Object.keys(user)
-for (const key of ourKeys) {
-    console.log(`${key}: ${user[key]}`)
+const  salariesArray = Object.values(salaries)
+let sum = 0;
+for (const item of salariesArray){
+  sum += item;
 }
+console.log(sum);
